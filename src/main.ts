3b60eee3cbe6a09521e9414d9676eca7173b8a05
Niels Bik <nielsrowinbik@gmail.com>
2021-01-22 16:41:08 +0100

    Add very basic Mirage interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createServer } from 'miragejs';
 
 if (process.env.NODE_ENV === 'development') {
-  // TODO: Create MirageJS server
+  createServer({
+    environment: 'development',
+
+    routes() {
+      this.namespace = 'api';
+
+      this.get('/radarr/movies', () => {
+        return [
+            { id: 1, name: 'Inception', year: 2010 },
+            { id: 2, name: 'Interstellar', year: 2014 },
+            { id: 3, name: 'Dunkirk', year: 2017 },
+            { id: 4, name: 'Tenet', year: 2020 }
+        ];
+      });
+    }
+  });
 }
 
 createApp(App).mount('#app');
