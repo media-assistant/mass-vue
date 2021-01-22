a10be8f684037ec348fca00af80f1a5eda68743a
Niels Bik <nielsrowinbik@gmail.com>
2021-01-22 14:57:13 +0100

    Prepare for MirageJS

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app');
+if (process.env.NODE_ENV === "development") {
+  // TODO: Create MirageJS server
+}
+
+createApp(App).mount("#app");
