6734f898a153e665e5de3a0988750ece600bafe7
Robert Groot <rorogro@gmail.com>
2021-02-06 20:38:50 +0100

    cs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
+import router from './plugins/router';
 import { useMirageJS } from './plugins/miragejs';
 
 import './index.css';
 import App from './App.vue';
-import router from './plugins/router';
 
 if (Number(import.meta.env.VITE_MIRAGE_ENABLED)) {
     useMirageJS();
