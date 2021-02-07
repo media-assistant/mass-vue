1c80912153614c87430bc1927696eeeec834c7d8
Niels Bik <nielsrowinbik@gmail.com>
2021-02-07 16:07:01 +0100

    Update MainPosterList to use HeroSection

--- src/components/MainPosterList.vue
+++ src/components/MainPosterList.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="wrapper">
-        <div class="grid grid-flow-col gap-10 overflow-auto px-16 pt-24 pb-12">
+    <HeroSection
+        src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
+        variant="compact"
+    >
+        <div class="grid grid-flow-col gap-10 overflow-auto px-16">
             <div v-if="loading">
                 Loading...
             </div>
@@ -11,28 +14,21 @@
                 :movie="movie"
             />
         </div>
-    </div>
+    </HeroSection>
 </template>
 
 <style lang="postcss" scoped>
-.wrapper {
-    background-position: center -50vh;
-    background-repeat: no-repeat;
-    /* TODO: Use dynamic image */
-    background-image: url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg);
-}
-
-.wrapper > :first-child {
-    backdrop-filter: blur(80px);
-    background-image: linear-gradient(to bottom, rgba(0,0,0,0.1) 0%, rgba(255,255,255,1) 40%);
+.grid {
     grid-auto-columns: calc(100vw - 8rem);
 }
 </style>
 
 <script setup lang="ts">
+import HeroSection from './HeroSection.vue';
+import MoviePosterLink from './MoviePosterLink.vue';
+
 import { useMovies } from '../compositions/movies';
 
-import MoviePosterLink from './MoviePosterLink.vue';
 
 const { movies, loading } = useMovies();
 </script>
