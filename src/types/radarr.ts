6ebf96117f91b50d65dbfc509726f7eb47d691bf
Niels Bik <nielsrowinbik@gmail.com>
2021-02-07 16:07:53 +0100

    Extend Radarr Movie types and (some of the) movies in the database

--- src/types/radarr.ts
+++ src/types/radarr.ts
@@ -1,6 +1,9 @@
 export type Movie = {
     id: number;
-    name: string;
+    name: string; // FIXME: Should be title
+    overview: string;
     poster: string;
+    runtime: number;
     year: number;
+    youTubeTrailerId: string;
 };
