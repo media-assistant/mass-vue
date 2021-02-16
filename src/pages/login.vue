106e8c2f8ac36a917c4769f256626d54bb7a4716
Niels Bik <nielsrowinbik@gmail.com>
2021-02-16 20:53:10 +0100

    Add styles to login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,44 +1,61 @@
 <template>
-    <div class="flex flex-col items-center">
-        <div>
-            <label
-                class="mr-4"
-                for="email"
-            >Email</label>
-            <input
-                v-model="email"
-                class="border"
-                type="text"
-                name="email"
+    <Section
+        class="flex flex-col h-screen justify-center"
+    >
+        <div class="-mt-24">
+            <h1
+                class="text-black dark:text-white text-3xl font-bold text-center mb-10"
             >
+                Media Assistant
+            </h1>
+            <div class="flex mb-6">
+                <label
+                    class="p-4 inline-block bg-gray-100 rounded-l-lg text-black font-semibold w-28"
+                    for="email"
+                >Email</label>
+                <input
+                    id="email"
+                    v-model="email"
+                    class="flex-auto bg-gray-100 rounded-r-lg focus:outline-none"
+                    type="text"
+                    name="email"
+                    placeholder="hi@example.com"
+                >
+            </div>
+            <div class="flex">
+                <label
+                    class="p-4 inline-block bg-gray-100 rounded-l-lg text-black font-semibold w-28"
+                    for="password"
+                >Password</label>
+                <input
+                    id="password"
+                    v-model="password"
+                    class="flex-auto bg-gray-100 rounded-r-lg focus:outline-none"
+                    type="password"
+                    name="password"
+                    placeholder="super-secure-password"
+                >
+            </div>
         </div>
-        <div>
-            <label
-                class="mr-4"
-                for="password"
-            >Password</label>
-            <input
-                v-model="password"
-                class="border"
-                type="password"
-                name="password"
-            >
-        </div>
-        <div>
-            <button
-                @click="login"
-            >
-                Login
-            </button>
-        </div>
-        <div>
-            <span>{{ error }}</span>
-        </div>
-    </div>
+    </Section>
+    <FixedFooter>
+        <CTAButton
+            :disabled="email !== '' && password !== ''"
+            @click="login"
+        >
+            Login
+        </CTAButton>
+        <!-- <div class="text-center">
+            Forgot password?
+        </div> -->
+    </FixedFooter>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import CTAButton from '../components/CTAButton.vue';
+import Section from '../components/Section.vue';
+import FixedFooter from '../components/FixedFooter.vue';
 import { useSession } from '../compositions/session';
 import { dataToInit, post } from '../plugins/fetch';
 import { HOME, TOKEN } from '../plugins/fetch/routes/mass-api';
