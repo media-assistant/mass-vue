08331f03bdf80650054e60f3eec6942c4eefd666
Niels Bik <nielsrowinbik@gmail.com>
2021-02-16 20:52:35 +0100

    Add FixedFooter component

---
+++ src/components/FixedFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="fixed bottom-0 left-0 right-0 px-6 pt-8 pb-6 bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
+        <slot />
+    </footer>
+</template>
