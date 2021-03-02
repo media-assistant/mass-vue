d70161a3471810c78a82b305f488333c03a2d604
Robert Groot <rorogro@gmail.com>
2021-03-02 20:57:35 +0100

    Removed console log

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -1,8 +1,6 @@
 import routes from 'voie-pages';
 import { createRouter, createWebHistory } from 'vue-router';
 
-console.log(routes);
-
 export default createRouter({
     history: createWebHistory(),
     routes
